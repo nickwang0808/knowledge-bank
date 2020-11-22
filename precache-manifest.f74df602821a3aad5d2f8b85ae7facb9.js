fbfc2bb4bf38af5cb9a48b7ca6217728fb99137b
Nick Wang <wang703003515@gmail.com>
2020-11-22 18:28:08 +0800

    Updates

---
+++ precache-manifest.f74df602821a3aad5d2f8b85ae7facb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f009757649de02e65c936dd219a64d",
+    "url": "/knowledge-bank/index.html"
+  },
+  {
+    "revision": "cb5a159d9ca82969fb17",
+    "url": "/knowledge-bank/static/css/main.4c1d1b51.chunk.css"
+  },
+  {
+    "revision": "25bd213430714311b69c",
+    "url": "/knowledge-bank/static/js/2.1187c615.chunk.js"
+  },
+  {
+    "revision": "cb5c9bef82b1b500bbeb6e747a258fde",
+    "url": "/knowledge-bank/static/js/2.1187c615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5a159d9ca82969fb17",
+    "url": "/knowledge-bank/static/js/main.23734380.chunk.js"
+  },
+  {
+    "revision": "ca06efdf45e45f04fae0",
+    "url": "/knowledge-bank/static/js/runtime-main.b14f5d29.js"
+  }
+]);
